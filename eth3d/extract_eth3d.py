074c3a2a1514b270d250c01f891600e2b1d43c5f
Joel Akiyoshi <akiyoshj@uci.edu>
2026-05-29 16:03:47 -0700

    add eth3d folder and unzipping script for downloaded images

---
+++ eth3d/extract_eth3d.py
@@ -0,0 +1,16 @@
+import py7zr
+
+def unzip_7z(archive_path, output_dir):
+    """
+    Extracts all files from a .7z archive.
+    """
+    try:
+        with py7zr.SevenZipFile(archive_path, mode='r') as archive:
+            archive.extractall(path=output_dir)
+        print(f"Successfully extracted {archive_path} to {output_dir}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Usage
+unzip_7z('multi_view_training_dslr_scan_eval.7z', 'eval_gt')
+unzip_7z('multi_view_training_dslr_undistorted.7z', 'highres_train')
